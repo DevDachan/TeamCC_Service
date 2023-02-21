05d3ea3572ad0fa98cf07b0227eb14a53a49418e
root <root@handongapp.cafe24.com>
2023-02-21 15:50:36 +0900

    feat: fix code

---
+++ .bash_history
@@ -0,0 +1,97 @@
+ls
+exit
+ls
+exit
+ls
+pm2 start main.js --watch
+pm2 log
+pm2 kill
+ls
+mysql -u root -p
+exit
+ls
+pm2 start main.js 
+pm2 log
+ls
+cd lib/
+ls
+vim db.js 
+pm2 log
+pm2 kill
+pm2 start main.js --watch
+cd ..
+pm2 start main.js --watch
+pm2 log
+pm2 kill
+sudo pm2 start main.js --watch
+exit
+ls
+mv default_background.jpg public/assets/img/default_background.jpg 
+ls
+cd lib/
+ls
+vim template_admin.js 
+ls
+vim template_user.js 
+vim template_admin.js 
+vim template_user.js 
+ls
+cd lib/
+ls
+vim template_admin.js 
+vim template_user.js 
+ls
+exit
+ls
+cd lib/
+ls
+vim template_admin.js 
+vim template_user.js 
+exit
+ls
+cd lib/
+ls
+vim template_admin.js 
+sl
+ls
+rm template_admin.js 
+ls
+cd ..
+ls
+exit
+ls
+mv template_admin.js lib/template_user.js 
+mv admin.css public/css/
+ls
+mv copy.png public/assets/
+ls
+cd public/
+ls
+cd css/
+ls
+cd ..
+ls
+cd assets/
+ls
+cd ..
+ls
+cd ..
+ls
+cd lib/
+ls
+exit
+ls
+mv template_admin.js lib/
+mv template_user.js lib/
+cd lib/
+ls
+vim template_admin.js 
+vim template_user.js 
+ls
+cd lib/
+ls
+vim template_admin.js 
+vim admin.js 
+vim template_admin.js 
+sudo mysql -u root -p
+exit
--- python/count_insert.py
+++ python/count_insert.py
@@ -8,20 +8,35 @@ now = datetime.now()
 cur_year = now.year
 cur_month = now.month
 cur_day = now.day
+count = 0
+
+
+con = pymysql.connect(host='localhost', user='root', password='Capstone2023',
+                       db='team_cc', charset='utf8')
+cur = con.cursor()
 
 
 while 1:
-    time.sleep(60)
-    if cur_year != now.year and cur_month != now.year and cur_day != now.day:
-        con = pymysql.connect(host='localhost', user='root', password='7894',
-                               db='team_cc', charset='utf8')
-        cur = con.cursor()
+    time.sleep(3)
+    
+    select_sql = "SELECT * FROM count WHERE year='"+str(now.year)+"' AND month = '"+str(now.month) +"' AND day ='"+str(now.day)+"'"
+    
+    print(select_sql)
+
+    cur.execute(select_sql)
+    result = cur.fetchall()
+
+    if len(result) == 0:
 
         sql = "INSERT INTO count VALUES('admin',0, '"+str(now.year)+"','"+str(now.month) +"','"+str(now.day)+"')"
         sql2 = "INSERT INTO count VALUES('user',0, '"+str(now.year)+"','"+str(now.month) +"','"+str(now.day)+"')"
+
+        print(sql)
+        print(sql2)
 
         cur.execute(sql)
         cur.execute(sql2)
 
         con.commit();
-        con.close()
+
+con.close()
